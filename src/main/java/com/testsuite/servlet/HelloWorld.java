89d731483e3d29f7ff7d8c931755f0e70cf83d84
D.Adams <duncanadamsxx@hotmail.co.uk>
2018-05-04 13:45:17 +0100

    Version 1
    Signed-off-by: D.Adams <duncanadamsxx@hotmail.co.uk>

--- src/main/java/com/testsuite/servlet/HelloWorld.java
+++ src/main/java/com/testsuite/servlet/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld extends HttpServlet {
 
     public void init() throws ServletException {
         // Do required initialization
-        message = "Hello World";
+        message = "Hello World # Jenkins - Test ";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
